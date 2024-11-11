572f2108f54aa62bcabb94ebfc49a863ed663c8c
jfrancini1905 <julia.francini@outlook.de>
2024-11-11 12:19:53 +0100

    first commit

---
+++ .vscode/main.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
+
+class StartScreen(Screen):
+    def on_enter(self):
+        # Wechsel nach 3 Sekunden zur MainScreen
+        Clock.schedule_once(self.switch_to_main, 3)
+
+    def switch_to_main(self, dt):
+        self.manager.current = 'main'
+
+class MainScreen(Screen):
+    pass
+
+class StellplatzScreen(Screen):
+    pass
+
+class VerwaltungsScreen(Screen):
+    pass
+
+class StellplatzverwaltungScreen(Screen):
+    pass
+
+
+class MyScreenManager(ScreenManager):
+    pass
+
+class MyApp(App):
+    def build(self):
+        sm = MyScreenManager()
+        sm.add_widget(StartScreen(name='start'))
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(StellplatzScreen(name='stellplatz'))
+        sm.add_widget(VerwaltungsScreen(name='verwaltung'))
+        return sm
+
+if __name__ == "__main__":
+    MyApp().run()
